c3f6d8c35bd3899815c330d0c2ac44a75180031b
Kelpering <tristan8272@gmail.com>
2023-02-01 09:18:04 -0700

    Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/* <tr scope="row">
+    <th>Name</th> (VARIABLE)
+    <td>TEST </td> (VARIABLE)
+</tr> */
+//Add this to the file storing the chat room for each new message.
+
+$(function() 
+{
+     
+     $("p").click(function() {
+        $(this).hide();
+     });
+     
+});
