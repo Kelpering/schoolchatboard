5cd1d9ccc6a26474173fb4419016d29f3eb4e769
Kelpering <tristan8272@gmail.com>
2023-02-02 08:55:06 -0700

    TURN IN

--- script.js
+++ script.js
@@ -4,11 +4,48 @@
 </tr> */
 //Add this to the file storing the chat room for each new message.
 
+var template = {
+
+   combine1 : "<tr scope=\"row\"><th>",
+   Name : "Unknown",
+   combine2 : "</th><td>",
+   Message : "Message", 
+   combine3 : "</td></tr>",
+   fullMessage : function()
+   {
+      return this.combine1+this.Name+this.combine2+this.Message+this.combine3;
+   }
+
+}
+
+localStorage.setItem("Messages", "TEST")
+var storage = localStorage.getItem("Messages");
+
 $(function() 
 {
      
-     $("p").click(function() {
-        $(this).hide();
+   $(".table-messages").html(storage)
+
+     $(".clear").click(function(){
+        localStorage.setItem("Messages", "")
+        $(".messages").html(localStorage.getItem("Messages"));
      });
-     
+
+     $(".send").click(function(){
+        template.Message = $("#sendMessage").value;
+        $("#sendMessage").value = "";
+        storage += template.fullMessage()
+        localStorage.setItem("Messages", )
+
+     });
+
+     $("#sendMessage").keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+
+        if(keycode=="13")
+        {
+            alert("submit")
+        }
+     });
+
 });
